5afcd400d5b3b02aa672b548a485aad49fa8c2a6
Iván López <lopez.ivan@gmail.com>
2018-09-27 17:40:40 +0200

    Use @Secured from new package

--- complete/src/main/java/example/micronaut/controllers/LoginAuthController.java
+++ complete/src/main/java/example/micronaut/controllers/LoginAuthController.java
@@ -2,7 +2,7 @@ package example.micronaut.controllers;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.security.Secured;
+import io.micronaut.security.annotation.Secured;
 import io.micronaut.views.View;
 
 import java.util.Collections;
